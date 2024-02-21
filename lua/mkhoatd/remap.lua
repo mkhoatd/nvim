63edbb6b3ba1c262a758a1adbe0b8a30b6cab982
mkhoatd <binnhatle@gmail.com>
2024-02-21 04:17:15 +0000

    updated keymap vim.ex

--- lua/mkhoatd/remap.lua
+++ lua/mkhoatd/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 local map = vim.keymap
 
-map.set("n", "<C-b>", vim.cmd.Ex)
+map.set("n", "<leader>b", vim.cmd.Ex)
 
 map.set("v", "J", ":m '>+1<CR>gv=gv")
 map.set("v", "K", ":m '<-2<CR>gv=gv")
