adda0cf7b37d5a931eaacb4e229171e5f3d1c1e7
mkhoatd <binnhatle@gmail.com>
2024-03-04 09:09:14 +0700

    Added git fugitive

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,6 @@ require("lazy").setup({
     } },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
